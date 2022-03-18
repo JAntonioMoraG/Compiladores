2abe9a467f22ce1fd120ce662733826881abda85
Jose Antonio Mora Guzman <90427927+JAntonioMoraG@users.noreply.github.com>
2022-03-17 22:42:12 -0600

    Funciones y procedimientos

---
+++ Practica 7/Entregable/mathc.txt
@@ -0,0 +1,43 @@
+/*
+DATOS
+*Mora Guzman Jose Antonio
+*Practica 7 funciones y procedimientos
+*Opcion calculadora vectores
+*fecha 1-dic-2021
+*grupo 3CM17
+*materia: compiladores
+*/
+#include <math.h>
+#include <errno.h>
+#include "vector_cal.h"
+
+extern int errno;
+double errcheck();
+
+Vector *magnitudVector(Vector *c)
+{
+	Vector *v;
+	v = creaVector(1);
+	double x = c->vec[0] * c->vec[0];
+	double y = c->vec[1] * c->vec[1];
+	double z = c->vec[2] * c->vec[2];
+	v->vec[0] = sqrt(x + y + z);
+
+	return v;
+}
+
+double errcheck(double d, char *s) /* revisar el resultado de la llamada
+                                        a la biblioteca */
+{
+	if (errno == EDOM)
+	{
+		errno = 0;
+		execerror(s, "argument out of donain");
+	}
+	else if (errno == ERANGE)
+	{
+		errno = 0;
+		execerror(s, "result out of range");
+	}
+	return d;
+}
---
+++ Practica 7/init.c
@@ -0,0 +1,68 @@
+/*
+DATOS
+*Mora Guzman Jose Antonio
+*Practica 7 funciones y procedimientos
+*Opcion calculadora vectores
+*fecha 1-dic-2021
+*grupo 3CM17
+*materia: compiladores
+*/
+#include "vector_cal.h"
+#include "y.tab.h"
+#include <math.h>
+#include <stdlib.h>
+
+extern Vector *magnitudVector();
+
+static struct
+{
+	/* Constantes */
+	char *name;
+	double cval;
+} consts[] = {
+	"PI", 3.14159265358979323846,
+	"E", 2.71828182845904523536,
+	"GAMMA", 0.57721566490153286060, /* Euler */
+	"DEG", 57.29577951308232087680,  /* grado/radian */
+	"PHI", 1.6180339887498948 * 820, /* proporcion dorada */
+	0, 0};
+
+static struct
+{ /*	Predefinidos */
+	char *name;
+	Vector *(*func)();
+} builtins[] =
+	{
+		"mag", magnitudVector,
+		0, 0};
+static struct
+{ /* palabras clave */
+	char *name;
+	int kval;
+} keywords[] = {
+	"proc", PROC,
+	"func", FUNC,
+	"return", RETURN,
+	"if", IF,
+	"else", ELSE,
+	"while", WHILE,
+	"print", PRINT,
+	"read", READ,
+	0, 0,
+};
+
+init() /* instalar constantes y predefinidos en la tabla */
+{
+	int i;
+	Symbol *s;
+
+	for (i = 0; consts[i].name; i++)
+		install(consts[i].name, VAR, NULL);
+	for (i = 0; builtins[i].name; i++)
+	{
+		s = install(builtins[i].name, BLTIN, NULL);
+		s->u.ptr = builtins[i].func;
+	}
+	for (i = 0; keywords[i].name; i++)
+		install(keywords[i].name, keywords[i].kval, NULL);
+}
